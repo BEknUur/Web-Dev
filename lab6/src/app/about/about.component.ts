1afd5ee3bf93938840eb97887bbfc230215d6bbc
Beknur <106554639+BEknUur@users.noreply.github.com>
2025-03-23 00:35:17 +0500

    lab7

--- lab6/src/app/about/about.component.ts
+++ lab6/src/app/about/about.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  selector:'app-about',
+  imports:[],
+  templateUrl:'./about.component.html',
+  styleUrl:'./about.component.css'
+
+
 })
-export class AboutComponent {
+export class AboutComponent{
 
 }
--- lab6/src/app/app.routes.ts
+++ lab6/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { AboutComponent } from './about/about.component';
 import { AlbumComponent } from './album/album.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
 import { PhotosComponent } from './photos/photos.component';
-import { AppComponent } from './app.component';  
 
 export const routes: Routes = [
     { path: 'about', component: AboutComponent },
